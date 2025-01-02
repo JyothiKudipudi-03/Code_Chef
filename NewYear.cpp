3f404cf6856397c9bc8b92b66eea7e8a31999aa5
Jyothi <22mh1a05b5@acoe.edu.in>
2025-01-02 20:06:19 +0530

    NewYear.cpp

---
+++ NewYear.cpp
@@ -0,0 +1,13 @@
+/*   Happy New Year!
+Currently, it is X:00 hours on December 31st  and you are wondering how many hours are left till midnight.
+For the purposes of this problem, we use a 24 hour system. So, X can range from 0 to 23,
+and you need to tell the number of hours left till 00:00 of the next day.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+        // your code goes here
+        int X;
+        cin >> X;
+        cout << 24 - X << endl;
+}
