ca6572b555177c639599ed2b367448e1960fa1ab
Jyothi <22mh1a05b5@acoe.edu.in>
2025-01-04 14:39:03 +0530

    Score.cpp

---
+++ Score.cpp
@@ -0,0 +1,25 @@
+/*  Is the Score Consistent
+Chef is watching a football match. The current score is A:B, that is, team 1 has scored A goals and team 2 has scored B goals. 
+Chef wonders if it is possible for the score to become C:D at a later point in the game (i.e. team 1 has scored C goals and team 2 has scored D goals).
+Can you help Chef by answering his question? */
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        // your code goes here
+        int t;
+        cin >> t;
+        while(t--){
+                int A,B,C,D;
+                cin >> A >> B >> C >> D;
+        if (C >= A && D >= B) {
+                cout << "POSSIBLE\n";
+        } else {
+                cout << "IMPOSSIBLE\n";
+        }
+        }
+}
