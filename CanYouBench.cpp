69f8d540a1ff041d5e881c75fb3ca97ef0fce52d
Jyothi <22mh1a05b5@acoe.edu.in>
2025-01-11 20:54:19 +0530

    CanYouBench.cpp

---
+++ CanYouBench.cpp
@@ -0,0 +1,15 @@
+/*  Can You Bench
+Chef goes to the gym daily and follow a progressive bench press routine. In the 1st  set, Chef will lift X kilograms.
+For each subsequent set, you increase the weight by 10 kilograms.
+Your task is to calculate the amount of weight Chef will bench in the 3rd set.  */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+        // your code goes here
+        int X;
+        cin >> X;
+        cout << X + 10 + 10 << endl;
+        return 0;
+
+}
